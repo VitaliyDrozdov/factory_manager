288fea24253b6ff633e56e189d15a30212cd96c1
Vitaliy Drozdov <vitdrzdv@mail.ru>
2024-09-14 08:15:36 +0300

    add models

---
+++ src/backend/directory/models.py
@@ -0,0 +1,31 @@
+from django.contrib.auth import get_user_model
+from django.db import models
+
+User = get_user_model()
+
+
+class Factory(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+
+    def __str__(self):
+        return self.name
+
+
+class Section(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    factory = models.ForeignKey(
+        Factory, related_name="sections", on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return self.name
+
+
+class Equipment(models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    sections = models.ManyToManyField(
+        Section, related_name="equipment", through="equipment_sections"
+    )
+
+    def __str__(self) -> str:
+        return self.name
