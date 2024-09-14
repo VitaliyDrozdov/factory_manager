95f8944b169323e6c03f79cde5be97c12af38274
Vitaliy Drozdov <vitdrzdv@mail.ru>
2024-09-14 10:56:49 +0300

    fix serializers

--- src/backend/directory/serializers.py
+++ src/backend/directory/serializers.py
@@ -3,19 +3,33 @@ from rest_framework import serializers
 
 
 class EquipmentSerializer(serializers.ModelSerializer):
+    sections = serializers.PrimaryKeyRelatedField(
+        queryset=Section.objects.all(), many=True
+    )
+
     class Meta:
         model = Equipment
-        fields = [
-            "name",
-        ]
+        fields = ["id", "name", "sections"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["sections"] = [section.id for section in instance.sections.all()]
+        return representation
 
 
 class SectionSerializer(serializers.ModelSerializer):
     equipment = EquipmentSerializer(many=True, read_only=True)
+    factory = serializers.PrimaryKeyRelatedField(queryset=Factory.objects.all())
 
     class Meta:
         model = Section
-        fields = ["name", "equipment"]
+        fields = ["id", "name", "factory", "equipment"]
+
+    def to_representation(self, instance):
+        representation = super().to_representation(instance)
+        representation["factory"] = instance.factory.id
+
+        return representation
 
 
 class FactorySerializer(serializers.ModelSerializer):
@@ -23,4 +37,4 @@ class FactorySerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Factory
-        fields = ["name", "sections"]
+        fields = ["id", "name", "sections"]
